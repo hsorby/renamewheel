412bd8db0928fe6faacdaf75021bfb188607aecc
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-09-05 15:29:03 +1200

    Fix path rendering.

--- tests/test_rename.py
+++ tests/test_rename.py
@@ -140,7 +140,7 @@ class RenameTestCase(unittest.TestCase):
         wheel = "simple_manylinux_demo-4.0-cp312-cp312-manylinux_2_5_x86_64.whl"
         target_dir = resource_path('..')
         target_file = os.path.join(target_dir, wheel)
-        expected_output = f"Copying {resource_path(wheel)!r} to {target_file!r}.\n"
+        expected_output = f"Copying '{resource_path(wheel)}' to {target_file!r}.\n"
 
         self._run_main_and_assert(['main.py', '-v', '-w', target_dir, wheel], 0, expected_output)
 
@@ -190,7 +190,7 @@ class RenameTestCase(unittest.TestCase):
         """Test successful rename when the target directory is the same as the source."""
         wheel = "simple_manylinux_demo-4.0-cp312-cp312-manylinux_2_5_x86_64.whl"
         target_dir = resource_path('die')
-        expected_output = f"Output directory {pathlib.Path(target_dir).resolve()!r} does not exist.\n"
+        expected_output = f"Output directory '{target_dir}' does not exist.\n"
         self._run_main_and_assert(['main.py', '-v', '-w', target_dir, wheel], 4, expected_output)
 
 
